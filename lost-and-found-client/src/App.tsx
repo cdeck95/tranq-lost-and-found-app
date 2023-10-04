bb8bb688035c5a80888395eb021a5afac0543796
Chris Deck <deckchris95@gmail.com>
2023-10-03 21:49:56 -0400

    prod URL

--- lost-and-found-client/src/App.tsx
+++ lost-and-found-client/src/App.tsx
@@ -21,8 +21,8 @@ export interface Disc {
   comments?: string | null;
 }
 
-//export const API_BASE_URL = 'https://lost-and-found-api-gl8z.onrender.com'; //production URL
-export const API_BASE_URL = 'http://127.0.0.1:3001'; // local testing
+export const API_BASE_URL = 'https://lost-and-found-api-gl8z.onrender.com'; //production URL
+//export const API_BASE_URL = 'http://127.0.0.1:3001'; // local testing
 
 
 function App() {
