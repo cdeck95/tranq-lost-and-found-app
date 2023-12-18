0ab4eaa5c8afece6db902a6b6969e9f037f0e63b
Chris Deck <deckchris95@gmail.com>
2023-12-18 10:59:54 -0500

    prod url

--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,8 @@ export interface Disc {
   dateSold?: string | null;
 }
 
-//export const API_BASE_URL = "https://lost-and-found-api-gl8z.onrender.com"; //production URL
-export const API_BASE_URL = "http://127.0.0.1:3001"; // local testing
+export const API_BASE_URL = "https://lost-and-found-api-gl8z.onrender.com"; //production URL
+//export const API_BASE_URL = "http://127.0.0.1:3001"; // local testing
 
 function App() {
   const [activeTab, setActiveTab] = useState("enterLostDisc"); // Default active tab
