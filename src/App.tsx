8bbd73a640e3cd97ff42b5ba28596d575d5db216
Chris Deck <deckchris95@gmail.com>
2024-02-19 15:25:04 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -43,9 +43,9 @@ export enum DiscStateString {
   Surrendered = "SURRENDERED",
 }
 
-export const API_BASE_URL =
-  "http://api-drn-env.eba-6jyxgrtu.us-east-1.elasticbeanstalk.com/"; //production URL without domain
-//export const API_BASE_URL = "https://api.discrescuenetwork.com"; //production URL
+//export const API_BASE_URL =
+("http://api-drn-env.eba-6jyxgrtu.us-east-1.elasticbeanstalk.com/"); //production URL without domain
+export const API_BASE_URL = "https://api.discrescuenetwork.com"; //production URL
 //export const API_BASE_URL = "http://127.0.0.1:3001"; // local testing
 
 function App() {
